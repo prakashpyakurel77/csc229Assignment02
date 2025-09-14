92c881220f7d2d88aaa0fc5b9e01d58bb443bd2c
prakashpyakurel77 <pyakp@farmingdale.edu>
2025-09-13 20:17:33 -0400

    Update Student.java

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -2,15 +2,57 @@ package com.mycompany.csc229_211review_lab02hw;
 
 /**
  *
- * @author MoaathAlrajab
+ * @Prakash Pyakurel
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+public class Student extends Person {
 
+	private double gpa;
+
+	private String address;
+
+	public  Student(String name, int age) {
+	super(name, (short) age); // cast age to short as required by the person
+
+	}
+	// getter for GPA
+
+	public double getGPA() {
+		return gpa;
+	}
+
+	//setter for GPA
+
+	public void setGpa (double gpa) {
+		this.gpa = gpa;
+	}
+		//now override 
+
+	@Override 
+	public String getAddress() {
+		return address;
+	}
+			/**
+     * Implementation of abstract setAddress() method from Person
+     * 
+     * @param address Address to set
+     */
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * Overridden toString() method to print student information
+     * 
+     * @return Formatted student info string
+     */
+    @Override
+    public String toString() {
+        return "Student{name='" + getName() + "', age=" + getAge()
+                + ", gpa=" + gpa + ", address='" + address + "'}";
+    }
+}
+		
+	
+	
 }
