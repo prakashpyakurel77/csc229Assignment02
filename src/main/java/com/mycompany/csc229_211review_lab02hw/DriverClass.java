349af0e60bd464e118582d08f7c076af97896380
prakashpyakurel77 <pyakp@farmingdale.edu>
2025-09-13 20:20:38 -0400

    Update DriverClass.java

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -5,24 +5,28 @@ package com.mycompany.csc229_211review_lab02hw;
  * @author MoaathAlrajab
  */
 public class DriverClass {
-	
-	public static void main(String[] args) {
-		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-		
-		// ToDo 7: Add a toString method for Student class
-		
-		Student std1= new Student("James", 20);
-		
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-		
-		System.out.println(std1);
-		
-		// ToDo 9: add comments and explain your code
-		
-		// ToDo 10: submit using a pull request.
-	}
+	public class DriverClass {
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Create a new student with name and age
+        Student std1 = new Student("Prakash", 30);
+
+        // Set GPA using user input
+        System.out.print("Enter GPA for " + std1.getName() + ": ");
+        double gpa = input.nextDouble();
+        std1.setGpa(gpa);
+
+        // set the address
+        input.nextLine(); // Consume newline
+        System.out.print("Enter address for " + std1.getName() + ": ");
+        String address = input.nextLine();
+        std1.setAddress(address);
+
+        // Print the student object
+        System.out.println(std1);
+
+        input.close();
+    }
 }
